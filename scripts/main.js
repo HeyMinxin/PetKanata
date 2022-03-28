9bb1c3ba2ba4bdb5bd66eea373e6f1026351ad5a
HeyMinxin <102530307+HeyMinxin@users.noreply.github.com>
2022-03-28 21:43:33 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var isPetting = false;
+var timeoutId;
+var popCount = 0;
+
+var Count = document.querySelector('h2');
+  
+function changeSrc()
+{
+    clearTimeout(timeoutId);
+    popCount++;
+    Count.textContent = popCount.toString();
+    document.getElementById("myKanata").src="images/Kanata2.png";
+    timeoutId = setTimeout(function() {
+        document.getElementById("myKanata").src="images/Kanata1.png" ;
+    }, 100); 
+}
+
